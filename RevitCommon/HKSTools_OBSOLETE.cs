dadcc314244c4c4ac7b27fefb6a2c7ab801d2589
tlogan <tlogan@hksinc.com>
2019-08-01 08:39:40 -0500

    Added temporary write to home class.

---
+++ RevitCommon/HKSTools_OBSOLETE.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitCommon
+{
+    [Obsolete("Temporarily replaced HKSTools.WriteToHome function to provide backwards compatibility for a transition period")]
+    public class HKS
+    {
+        public static void WriteToHome(string commandName, string appVersion, string userName)
+        {
+            // Do nothing
+        }
+    }
+}
